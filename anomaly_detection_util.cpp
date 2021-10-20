1a4ce957d64680e39f731460283806d9ea8bd849
inbal <inbal.apt@live.biu.ac.il>
2021-10-20 11:42:39 -0700

    f

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -1,10 +1,16 @@
 //
-// Created by Noa Leshem and Inbal Apt on 10/20/21.
+// Created by Noa Leshem - 314665415, and Inbal Apt - 207031006 on 10/20/21.
 //
 #include <iostream>
 #include "anomaly_detection_util.h"
 #include <cmath>
 
+/**
+ * this function gets an array of floats and its size, and calculate its average.
+ * @param x
+ * @param size
+ * @return float
+ */
 float E(float* x, int size) {
     float variable = 0;
     for (int i = 0; i < size; i++) {
@@ -13,14 +19,21 @@ float E(float* x, int size) {
     return variable;
 }
 
+/**
+ * this function gets an arrays of x floats and y floats and those sizes, and calculate the average of their multiply.
+ * @param x
+ * @param y
+ * @param size
+ * @return float
+ */
 float E(float* x, float* y, int size) {
     float variable;
-    for (int i=0;i<size; i++){
-        variable += x[i]*y[i] / size;
+    for (int i = 0; i < size; i++){
+        variable += x[i] * y[i] / size;
     }
 }
 
-// returns the variance of X and Y
+
 float var(float* x, int size) {
     float variable = E(x, size);
     float sum = 0;
@@ -29,26 +42,39 @@ float var(float* x, int size) {
     }
     return sum;
 }
-// returns the covariance of X and Y
+
 float cov(float* x, float* y, int size) {
     return E(x, y, size) - E(x, size) * E(y, size);
 }
-// returns the Pearson correlation coefficient of X and Y
+
 float pearson(float* x, float* y, int size) {
     float xVarSqrt = sqrt(var(x, size));
     float yVarSqrt = sqrt(var(y, size));
     return cov(x, y, size) / (xVarSqrt * yVarSqrt);
 
 }
-// performs a linear regression and return s the line equation
+
 Line linear_reg(Point** points, int size) {
-    float a = cov(*points, *points, size) / var(points[0], size);
+    float pointsX[size];
+    float pointsY[size];
+    for (int i = 0; i < size; i++) {
+        pointsX[i] = points[i]->x;
+        pointsY[i] = points[i]->y;
+    }
+    float a = cov(pointsX, pointsY, size) / var(pointsX, size);
+    float aveX = E(pointsX, size);
+    float aveY = E(pointsY, size);
+    float b = aveY - aveX * a;
+    Line line;
+    line.a = a;
+    line.b = b;
+    return line;
 }
-// returns the deviation between point p and the line equation of the points
+
 float dev(Point p,Point** points, int size) {
 return 0;
 }
-// returns the deviation between point p and the line
+
 float dev(Point p,Line l) {
 return 0;
 }
--- anomaly_detection_util.h
+++ anomaly_detection_util.h
@@ -1,15 +1,34 @@
 //
-// Created by Noa Leshem and Inbal Apt on 10/20/21.
+// Created by Noa Leshem - 314665415, and Inbal Apt - 207031006 on 10/20/21.
 //
 
 #ifndef AS1_ANOMALY_DETECTION_UTIL_H
 #define AS1_ANOMALY_DETECTION_UTIL_H
 
-// returns the variance of X and Y
+/**
+ * this function returns the variance of X and Y.
+ * @param x
+ * @param size
+ * @return float
+ */
 float var(float* x, int size);
-// returns the covariance of X and Y
+
+/**
+ * returns the covariance of X and Y.
+ * @param x
+ * @param y
+ * @param size
+ * @return float
+ */
 float cov(float* x, float* y, int size);
-// returns the Pearson correlation coefficient of X and Y
+
+/**
+ * returns the Pearson correlation coefficient of X and Y.
+ * @param x
+ * @param y
+ * @param size
+ * @return float
+ */
 float pearson(float* x, float* y, int size);
 class Line{
 public:
@@ -25,11 +44,30 @@ public:
     float x,y;
     Point(float x, float y):x(x),y(x){}
 };
-// performs a linear regression and return s the line equation
+
+/**
+ * performs a linear regression and return s the line equation.
+ * @param points
+ * @param size
+ * @return Line
+ */
 Line linear_reg(Point** points, int size);
-// returns the deviation between point p and the line equation of the points
+
+/**
+ * returns the deviation between point p and the line equation of the points.
+ * @param p
+ * @param points
+ * @param size
+ * @return float
+ */
 float dev(Point p,Point** points, int size);
-// returns the deviation between point p and the line
+
+/**
+ * returns the deviation between point p and the line.
+ * @param p
+ * @param l
+ * @return float
+ */
 float dev(Point p,Line l);
 
 #endif //AS1_ANOMALY_DETECTION_UTIL_H
