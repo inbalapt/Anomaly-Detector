d38ab1260326e5b6ced254d2da7af15330151f6b
noalesh <noaleshem99@gmail.com>
2021-10-20 00:56:11 -0700

    g

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hellooooooo, World!" << std::endl;
     return 0;
 
 }
