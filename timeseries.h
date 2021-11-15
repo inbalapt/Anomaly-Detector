9dc69ee0ef14cee921c1a1a3553e7e78bdaf04c8
inbal <inbal.apt@live.biu.ac.il>
2021-11-15 00:42:16 -0800

    j

--- timeseries.h
+++ timeseries.h
@@ -1,16 +1,20 @@
 
+#include <vector>
+#include <string>
 
 #ifndef TIMESERIES_H_
 #define TIMESERIES_H_
 
 using namespace std;
-
+    vector<pair<string, vector<float>>> vector;
 class TimeSeries{
 
 public:
 
-    TimeSeries(const char* CSVfileName){
+    TimeSeries(const char* CSVfileName) {
+
     }
+
 
 };
 
