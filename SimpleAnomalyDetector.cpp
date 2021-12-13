3c0967b943b1586e981785b78c8dbe785b48c479
noaLeshem2 <noaleshem99@gmail.com>
2021-12-13 14:58:28 +0200

    hi

--- SimpleAnomalyDetector.cpp
+++ SimpleAnomalyDetector.cpp
@@ -53,7 +53,7 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries &ts) {
             for (int k = 0; k < rows; k++) {
                 float x = table[i].second[k];
                 float y = table[col].second[k];
-                float distance = dev(Point(x,y), line);
+                float distance = dev(Point(x, y), line);
                 // The highest threshold.
                 if (distance > threshold) {
                     threshold = distance;
@@ -66,7 +66,7 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries &ts) {
                     highestCor,
                     line,
                     threshold
-                };
+            };
             // Add to the list of cf
             this->cf.push_back(core);
         }
@@ -77,22 +77,22 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries &ts) {
  * This function gets time series and find anomalies, if there are, according to the correlative features
  * that we found in the learning level (learnNormal function), set them in a vector, and returns it.
  */
-vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries& ts){
+vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries &ts) {
     vector<AnomalyReport> report;
     // Running over the correlated features that we found in learnNormal
-    for(const correlatedFeatures& corF: this->getNormalModel()) {
+    for (const correlatedFeatures &corF: this->getNormalModel()) {
         string fea1 = corF.feature1;
         string fea2 = corF.feature2;
         // gets the current values of the features
         const vector<float> values1 = ts.get_feature_by_string(fea1);
         const vector<float> values2 = ts.get_feature_by_string(fea2);
-        int size = (int)values1.size();
+        int size = (int) values1.size();
         // Running over the values of the features
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             float x = values1[i];
             float y = values2[i];
             // Check if the deviation of this point is bigger than the threshold
-            if (dev(Point(x,y), corF.lin_reg) >= corF.threshold) {
+            if (dev(Point(x, y), corF.lin_reg) >= corF.threshold) {
                 // Save the description of the features that have a deviation
                 string description = fea1 + "-";
                 description += fea2;
